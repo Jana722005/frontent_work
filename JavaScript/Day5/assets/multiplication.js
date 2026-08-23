e964abf531beaaaad7a6b7aa539b6685e107efaa
JANARTHANAN R <janarthananj236@gmail.com>
2026-08-23 23:21:32 +0530

    latest commit js

---
+++ JavaScript/Day5/assets/multiplication.js
@@ -0,0 +1,7 @@
+let a = 5;
+
+for(let i = 1; i<= 10; i++){
+    let b = a * i;
+    document.write(a + "x" + i + "=" + b + "<br>");
+    console.log(a + "x" + i + "=" + b);
+}
---
+++ JavaScript/Day5/assets/number_2.js
@@ -0,0 +1,5 @@
+for(let i = 1; i <= 20; i++){
+    document.write(i + "<br>");
+    console.log(i);
+    
+}
